2cfe892dc41945269f17a772eabe78ca073e779e
Daniel Gonzalez <danielhumgon@gmail.com>
2023-07-19 10:36:40 -0400

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,166 @@
-import Image from 'next/image'
+import Image from "next/image";
+
+// Assets
+import logo from "./assets/logo.png";
+// import appstore from './assets/appstore.png'
+import macstore from "./assets/macstore.png";
+import phone1 from "./assets/phone1.jpeg";
+import phone6 from "./assets/phone6.jpeg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <main className="bg-gray-50">
+      {/* Hero Section */}
+      <div className="flex flex-col gap-4 p-6">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          className="w-24 h-24 m-auto mt-4 p-4 border border-gray-300 rounded-2xl shadow-md"
+          src={logo}
+          alt="logo"
         />
+        <h1 className="text-2xl text-center font-bold p-3 text-gray-900 sm:text-4xl sm:w-3/4 sm:m-auto">
+          Your number one destination to all smartphone related info - be smart!
+        </h1>
+        <div className="sm:flex sm:w-2/3 sm:m-auto">
+          <Image
+            className="w-40 h-12 m-auto sm:w-48 sm:h-14"
+            src={macstore}
+            alt="app store logo"
+          />
+          <Image
+            className="w-40 h-12 m-auto invisible sm:visible sm:w-48 sm:h-14"
+            src={macstore}
+            alt="mac store logo"
+          />
+        </div>
+        
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Contents Section */}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* Card 1 */}
+      <div className="flex flex-col border border-gray-200 rounded-2xl shadow-md m-auto w-11/12 p-8 mb-12">
+        <p className="text-lg">
+          <span className="font-bold">Lorem</span>{" "}
+          <span className="text-gray-500">
+            ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            consequatur fuga repellat esse aliquid accusantium commodi facilis.
+          </span>
+        </p>
+        <Image className="m-auto" src={phone1} alt="phone 1" />
+      </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+      {/* Card 2 */}
+      <div className="flex flex-col border border-gray-200 rounded-2xl shadow-md m-auto w-11/12 p-8 mb-12">
+        <p className="text-lg">
+          <span className="font-bold">Lorem</span>{" "}
+          <span className="text-gray-500">
+            ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            consequatur fuga repellat esse aliquid accusantium commodi facilis.
+          </span>
+        </p>
+        <Image className="m-auto" src={phone1} alt="phone 1" />
+      </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+      {/* Card 3 */}
+      <div className="flex flex-col border border-gray-200 rounded-2xl shadow-md m-auto w-11/12 p-8 mb-12">
+        <p className="text-lg">
+          <span className="font-bold">Lorem</span>{" "}
+          <span className="text-gray-500">
+            ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            consequatur fuga repellat esse aliquid accusantium commodi facilis.
+          </span>
+        </p>
+        <Image className="m-auto" src={phone1} alt="phone 1" />
+      </div>
+
+      {/* Card 4 */}
+      <div className="flex flex-col border border-gray-200 rounded-2xl shadow-md m-auto w-11/12 p-8 mb-12">
+        <p className="text-lg">
+          <span className="font-bold">Lorem</span>{" "}
+          <span className="text-gray-500">
+            ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            consequatur fuga repellat esse aliquid accusantium commodi facilis.
+          </span>
+        </p>
+        <Image className="m-auto" src={phone1} alt="phone 1" />
+      </div>
+
+      {/* Card 5 */}
+      <div className="flex flex-col border border-gray-200 rounded-2xl shadow-md m-auto w-11/12 p-8 mb-12">
+        <p className="text-lg">
+          <span className="font-bold">Lorem</span>{" "}
+          <span className="text-gray-500">
+            ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            consequatur fuga repellat esse aliquid accusantium commodi facilis.
+          </span>
+        </p>
+        <Image className="m-auto" src={phone1} alt="phone 1" />
+      </div>
+
+      {/* Card 6 */}
+      <div className="flex flex-col border border-gray-200 rounded-2xl shadow-md m-auto w-11/12 p-8 mb-12">
+        <div className="text-lg">
+          <h2 className="font-bold">Get on iPad and macOS</h2>
+          <span className="text-gray-500">
+            Available for all your devices. Never be disconnected in any of your
+            devices.
+          </span>
+          <Image
+            className="w-40 h-12 mt-4"
+            src={macstore}
+            alt="app store logo"
+          />
+          <Image
+            className="w-40 h-12 mt-3"
+            src={macstore}
+            alt="mac store logo"
+          />
+        </div>
+        <Image className="m-auto" src={phone6} alt="phone 6" />
+      </div>
+
+      {/* FAQ Section */}
+
+      <div className="flex flex-col text-lg mb-8">
+        {/* Question 1 */}
+        <div className="p-5">
+          <h2 className="font-bold mb-3">Do you have any social networks?</h2>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde
+            itaque officia dicta quod, assumenda nulla, voluptatum in
+            repellendus soluta.
           </p>
-        </a>
+        </div>
+
+        {/* Question 2 */}
+        <div className="p-5">
+          <h2 className="font-bold mb-3">Do you have any social networks?</h2>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde
+            itaque officia dicta quod, assumenda nulla, voluptatum in
+            repellendus soluta.
+          </p>
+        </div>
+
+        {/* Question 3 */}
+        <div className="p-5">
+          <h2 className="font-bold mb-3">Do you have any social networks?</h2>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil unde
+            itaque officia dicta quod, assumenda nulla, voluptatum in
+            repellendus soluta.
+          </p>
+        </div>
+
+        {/* Footer */}
+
+        <footer className="text-center p-4">
+          <p className="text-gray-400 text-xs">
+            &copy; Copyright {new Date().getFullYear()} All rights
+            reserved.
+          </p>
+        </footer>
       </div>
     </main>
-  )
+  );
 }
